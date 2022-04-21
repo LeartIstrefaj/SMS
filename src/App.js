c57f8f37e6bbb53b5fae28655c56b51f39fa9374
LeartIstrefaj <leartistrefaj02@gmail.com>
2022-04-21 15:35:12 +0200

    other link functionalization

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import {
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
 import { Home } from "./components/Home";
+import { Location } from "./components/Location";
+
 // imports other versions bootstraps
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
@@ -22,14 +24,14 @@ function App() {
 
         <Navbar bg="light" expand="lg">
           <Container>
-            <Navbar.Brand href="#">SMS</Navbar.Brand>
+            <Navbar.Brand href="/">SMS</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
                 <Nav.Link as={Link} to="/">Home</Nav.Link>
                 <Nav.Link as={Link} to="/about">About</Nav.Link>
                 <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
-                <Nav.Link as={Link} to="/review">Review</Nav.Link>
+                <Nav.Link as={Link} to="/location">Location</Nav.Link>
                 <NavDropdown title="Registration" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">Sign up</NavDropdown.Item>
@@ -44,6 +46,7 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/" element={<Home />} />
+            <Route path="/location" element={<Location/>} />
           </Routes>
         </div>
       </div>
---
+++ src/components/Location.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Location = () => {
+    return (
+        <div>
+            <h3>This is Location Page.</h3>
+        </div>
+    );
+}
