2b32b13e71cc3cb49e7760ce5ab296b451e37378
LeartIstrefaj <leartistrefaj02@gmail.com>
2022-04-21 14:46:28 +0200

    continue implement navigation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 import './App.css';
-import {Navbar,Nav,Container,NavDropdown} from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { Home } from "./components/Home";
+// imports other versions bootstraps
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+
+
 function App() {
   return (
-    <div className='App'>
 
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#">SMS</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">About</Nav.Link>
-              <Nav.Link href="#link">Contact Us</Nav.Link>
-              <Nav.Link href="#link">Other</Nav.Link>
-              <NavDropdown title="Registration" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Sign up</NavDropdown.Item>
-                
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
 
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#">SMS</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/about">About</Nav.Link>
+                <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+                <Nav.Link as={Link} to="/review">Review</Nav.Link>
+                <NavDropdown title="Registration" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Sign up</NavDropdown.Item>
+
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Contact = () => {
+    return (
+        <div>
+            <h3>This is my contact section.</h3>
+        </div>
+    );
+}
