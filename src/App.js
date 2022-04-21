9829c87816fbe6f5dd9bd4befda61cd31c5700b5
LeartIstrefaj <leartistrefaj02@gmail.com>
2022-04-21 14:07:15 +0200

    init- create navbar basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import {Navbar,Nav,Container,NavDropdown} from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 function App() {
   return (
+    <div className='App'>
 
-      
-    
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">SMS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link href="#link">Contact Us</Nav.Link>
+              <Nav.Link href="#link">Other</Nav.Link>
+              <NavDropdown title="Registration" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Sign up</NavDropdown.Item>
+                
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+
   );
 }
 
