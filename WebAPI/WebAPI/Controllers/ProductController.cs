f6f2c5950c94fe88d76a0032fa88a863f9f88c8c
LeartIstrefaj <leartistrefaj02@gmail.com>
2022-05-04 23:55:03 +0200

    init

---
+++ WebAPI/WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using WebAPI.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+
+        public ProductController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                 select ProductId, ProductName, Category,
+                 convert(varchar(10),DateOfJoining,120) as DateOfJoining
+                 ,PhotoFileName
+                  from dbo.Product
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMSCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Product pro)
+        {
+            string query = @"
+                insert into dbo.Product 
+                (ProductName,Category,DateOfJoining,PhotoFileName)
+                values
+                (
+                '" + pro.ProductName + @"'
+                ,'" + pro.Category + @"'
+                ,'" + pro.DateOfJoining + @"'
+                ,'" + pro.PhotoFileName + @"'
+                )
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMSCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+                return new JsonResult("Added Successfully");
+            }
+        }
+
+        [HttpPut]
+        public JsonResult Put(Product pro)
+        {
+            string query = @"
+               update dbo.Product set 
+               ProductName = '" + pro.ProductName + @"'
+               ,Category = '" + pro.Category + @"'
+               ,DateOfJoining = '" + pro.DateOfJoining + @"'
+               where ProductId = " + pro.ProductId + @"
+               ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMSCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+                return new JsonResult("Update Successfully");
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+               delete from dbo.Product
+               where ProductId = " + id + @"
+               ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMSCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+                return new JsonResult("Deleted Successfully");
+            }
+        }
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                return new JsonResult(filename);
+            }
+            catch(Exception)
+            {
+                return new JsonResult("anonymous.png");
+            }
+        }
+
+        [Route("GetAllCategoryNames")]
+        public JsonResult GetAllCategoryNames()
+        {
+            string query = @"
+                 select CategoryName from dbo.Category
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMSCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        
+        }
+    }
+}
