11f4935abbaa23725da60dfa3e57e6349074307b
Ndriqim <ndriqimib@gmail.com>
2022-05-12 17:04:16 +0200

    login and registration form

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,9 +1,50 @@
 import React from "react";
 
 export const Login = () => {
-    return (
-        <div className="box-title">
-            <h3 className="title">Welcome to the Login Form</h3>
+    return (<form>
+        <h3>Sign In</h3>
+
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
         </div>
-    );
+
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
+
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Forgot <a href="#">password?</a>
+        </p>
+      </form>
+    )
+    
+    ;
 }
