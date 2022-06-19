011068a2442ec14fa247f629d0bc0f7bc6219cc1
LeartIstrefaj <leartistrefaj02@gmail.com>
2022-06-19 18:03:58 +0200

    init

--- front/src/components/Furnitori.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Furnitori = () => {
-    return (
-        <div>
-            <h1>Furnitori page</h1>
-        </div>  
-    );
-}
--- front/src/components/Stoku.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Stoku = () => {
-    return (
-        <div>
-            <h1>Stoku page</h1>
-        </div>  
-    );
-}
