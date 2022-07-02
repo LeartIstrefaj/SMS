55567b9f1d79f4c10c5a41412f9ae5d697d25585
LeartIstrefaj <leartistrefaj02@gmail.com>
2022-07-02 10:56:27 +0200

    users

--- front/User/src/components/Tv.js
+++ front/User/src/components/Tv.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import { Table } from 'react-bootstrap';
 
-import { Button, ButtonToolbar } from 'react-bootstrap';
-
-import { AddTvModal } from '../components/AddTvModal';
-import { EditTvModal } from '../components/EditTvModal';
-
 export class Tv extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { tvv: [], addModalShow: false, editModalShow: false }
+    this.state = { tvv: [] }
   }
 
   refreshList() {
@@ -29,22 +24,10 @@ export class Tv extends Component {
     this.refreshList();
   }
 
-  deleteTv(tid) {
-    if (window.confirm('Are you sure?')) {
-      fetch('http://localhost:36468/api/tv/' + tid, {
-        method: 'DELETE',
-        header: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
-    }
-  }
 
   render() {
-    const { tvv, tvid, tvname, serialkey, price, type } = this.state;
-    let addModalClose = () => this.setState({ addModalShow: false });
-    let editModalClose = () => this.setState({ editModalShow: false });
+    const { tvv } = this.state;
+
     return (
       <div className='container'>
         <br />
@@ -52,45 +35,18 @@ export class Tv extends Component {
         <Table className="mt-4" striped bordered hover size="sm">
           <thead>
             <tr>
-              <th>TV ID</th>
               <th>TV Name</th>
-              <th>Serial Key</th>
-              <th>Price</th>
               <th>Type</th>
-              <th>Options</th>
+              <th>Price</th>
             </tr>
           </thead>
           <tbody>
             {tvv.map(tv =>
               <tr key={tv.TvId}>
-                <td>{tv.TvId}</td>
                 <td>{tv.TvName}</td>
-                <td>{tv.SerialKey}</td>
-                <td>{tv.Price}</td>
                 <td>{tv.Type}</td>
+                <td>{tv.Price}</td>
                 <td>
-                  <ButtonToolbar>
-                    <Button className="mr-2" variant="warning"
-                      onClick={() => this.setState({
-                        editModalShow: true,
-                        tvid: tv.TvId, tvname: tv.TvName, serialkey: tv.SerialKey, price: tv.Price, type: tv.Type,
-                      })}>
-                      Edit
-                    </Button>
-
-                    <Button className="mr-2" variant="danger"
-                      onClick={() => this.deleteTv(tv.TvId)}>
-                      Delete
-                    </Button>
-
-                    <EditTvModal show={this.state.editModalShow}
-                      onHide={editModalClose}
-                      tvid={tvid}
-                      tvname={tvname}
-                      serialkey={serialkey}
-                      price={price}
-                      type={type} />
-                  </ButtonToolbar>
 
                 </td>
 
@@ -99,14 +55,6 @@ export class Tv extends Component {
 
         </Table>
 
-        <ButtonToolbar>
-          <Button variant='primary'
-            onClick={() => this.setState({ addModalShow: true })}>
-            Add TVs</Button>
-
-          <AddTvModal show={this.state.addModalShow}
-            onHide={addModalClose} />
-        </ButtonToolbar>
       </div>
     )
   }
